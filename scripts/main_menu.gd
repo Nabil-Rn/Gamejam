052b1c3e7bf254897b4fee6898e93dfd23300359
Nabil-Rn <98413966+Nabil-Rn@users.noreply.github.com>
2025-01-19 12:47:50 -0500

    empty

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -21,3 +21,7 @@ func _on_how_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credit_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credit.tscn")
