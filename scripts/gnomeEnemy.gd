002d9e336b2b67681ff91ad642c468f938441750
Nabil-Rn <98413966+Nabil-Rn@users.noreply.github.com>
2025-01-19 12:50:24 -0500

    enemy code

---
+++ scripts/gnomeEnemy.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+class_name  gnomeEnemy
+
+var speed = 200
+var is_gnome_chase = false
+var player = null
+var health = 80
+
+var dead: bool = false
+var taking_damage: bool = false
+var damage_to_deal = 20
+
+func _physics_process(delta: float) -> void:		
+	if not is_on_floor():
+		velocity += get_gravity() * delta * 1.5
+	
+	if is_gnome_chase:
+		var direction = (player.position - position).normalized()
+		velocity.x = direction.x * speed
+		#$AnimatedSprite2D.play()
+	
+	if taking_damage:
+		health -= 30
+		
+	if health == 0:
+		dead = true
+	move_and_slide()
+
+func _attack():
+	print("test")
+	
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	player = body
+	is_gnome_chase = true
+	
---
+++ scripts/goblinEnemy.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+class_name  goblinEnemy
+
+var speed = 200
+var is_goblin_detect = false
+var player = null
+var health = 80
+
+var dead: bool = false
+var taking_damage: bool = false
+var damage_to_deal = 20
+
+func _physics_process(delta: float) -> void:		
+	if is_goblin_detect:
+		_attack()
+		#$AnimatedSprite2D.play()
+	
+	if taking_damage:
+		health -= 30
+		
+	if health == 0:
+		dead = true
+
+func _attack():
+	print("test")
+	
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	player = body
+	is_goblin_detect = true
+	
